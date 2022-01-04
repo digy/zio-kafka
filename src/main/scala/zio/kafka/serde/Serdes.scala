23bc7d6ecda116efd4d49d52970b13f197ecbc68
Sergejs Pugacs <noreply>
2022-01-04 09:32:53 +0100

    Removing environment from the Serde

--- src/main/scala/zio/kafka/serde/Serdes.scala
+++ src/main/scala/zio/kafka/serde/Serdes.scala
@@ -5,28 +5,28 @@ import java.util.UUID
 
 import org.apache.kafka.common.serialization.{ Serde => KafkaSerde, Serdes => KafkaSerdes }
 import org.apache.kafka.common.header.Headers
-import zio.{ RIO, Task }
+import zio.Task
 
 private[zio] trait Serdes {
-  lazy val long: Serde[Any, Long]     = convertPrimitiveSerde(KafkaSerdes.Long()).inmap(Long2long)(long2Long)
-  lazy val int: Serde[Any, Int]       = convertPrimitiveSerde(KafkaSerdes.Integer()).inmap(Integer2int)(int2Integer)
-  lazy val short: Serde[Any, Short]   = convertPrimitiveSerde(KafkaSerdes.Short()).inmap(Short2short)(short2Short)
-  lazy val float: Serde[Any, Float]   = convertPrimitiveSerde(KafkaSerdes.Float()).inmap(Float2float)(float2Float)
-  lazy val double: Serde[Any, Double] = convertPrimitiveSerde(KafkaSerdes.Double()).inmap(Double2double)(double2Double)
-  lazy val string: Serde[Any, String] = convertPrimitiveSerde(KafkaSerdes.String())
-  lazy val byteArray: Serde[Any, Array[Byte]] = convertPrimitiveSerde(KafkaSerdes.ByteArray())
-  lazy val byteBuffer: Serde[Any, ByteBuffer] = convertPrimitiveSerde(KafkaSerdes.ByteBuffer())
-  lazy val uuid: Serde[Any, UUID]             = convertPrimitiveSerde(KafkaSerdes.UUID())
+  lazy val long: Serde[Long]     = convertPrimitiveSerde(KafkaSerdes.Long()).inmap(Long2long)(long2Long)
+  lazy val int: Serde[Int]       = convertPrimitiveSerde(KafkaSerdes.Integer()).inmap(Integer2int)(int2Integer)
+  lazy val short: Serde[Short]   = convertPrimitiveSerde(KafkaSerdes.Short()).inmap(Short2short)(short2Short)
+  lazy val float: Serde[Float]   = convertPrimitiveSerde(KafkaSerdes.Float()).inmap(Float2float)(float2Float)
+  lazy val double: Serde[Double] = convertPrimitiveSerde(KafkaSerdes.Double()).inmap(Double2double)(double2Double)
+  lazy val string: Serde[String] = convertPrimitiveSerde(KafkaSerdes.String())
+  lazy val byteArray: Serde[Array[Byte]] = convertPrimitiveSerde(KafkaSerdes.ByteArray())
+  lazy val byteBuffer: Serde[ByteBuffer] = convertPrimitiveSerde(KafkaSerdes.ByteBuffer())
+  lazy val uuid: Serde[UUID]             = convertPrimitiveSerde(KafkaSerdes.UUID())
 
-  private[this] def convertPrimitiveSerde[T](serde: KafkaSerde[T]): Serde[Any, T] =
-    new Serde[Any, T] {
+  private[this] def convertPrimitiveSerde[T](serde: KafkaSerde[T]): Serde[T] =
+    new Serde[T] {
       val serializer   = serde.serializer()
       val deserializer = serde.deserializer()
 
-      override def deserialize(topic: String, headers: Headers, data: Array[Byte]): RIO[Any, T] =
+      override def deserialize(topic: String, headers: Headers, data: Array[Byte]): Task[T] =
         Task(deserializer.deserialize(topic, headers, data))
 
-      override def serialize(topic: String, headers: Headers, value: T): RIO[Any, Array[Byte]] =
+      override def serialize(topic: String, headers: Headers, value: T): Task[Array[Byte]] =
         Task(serializer.serialize(topic, headers, value))
     }
 }
